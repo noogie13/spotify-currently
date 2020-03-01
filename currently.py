88a133b775a6ecfb27ff342e0da41e77c705e794
Solomon Bloch <solomonbloch@gmail.com>
2020-03-01 16:19:17 -0500

    Remove ID, secret, redirect.

--- currently.py
+++ currently.py
@@ -2,15 +2,24 @@
 import OAUTH
 import requests
 
+id = 'ENTER ID'
+
+secret = 'ENTER SECRET'
+
+redirect = 'ENTER REDIRECT'
+
 scopes = 'user-read-playback-state user-read-currently-playing user-modify-playback-state'
 
-user = OAUTH.OAUTH(scopes)
+
+user = OAUTH.OAUTH(id, secret, redirect, scopes)
 
 isplaying = requests.get('https://api.spotify.com/v1/me/player',
-                         headers={'Authorization':'Bearer ' + user.token['access_token']}).text
+                         headers={'Authorization':'Bearer '
+                                  + user.token['access_token']}).text
 
 currently = requests.get('https://api.spotify.com/v1/me/player/currently-playing',
-                         headers={'Authorization':'Bearer ' + user.token['access_token']}).json()
+                         headers={'Authorization':'Bearer '
+                                  + user.token['access_token']}).json()
 
 print(currently['item']['artists'][0]['name'] + ' - ' + currently['item']['album']['name'])
 
